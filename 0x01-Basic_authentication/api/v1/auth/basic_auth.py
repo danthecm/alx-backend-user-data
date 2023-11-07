9ce661bdcc838db0d45315d72c6bac1de30edd89
danthecm <etchiedaniel1@gmail.com>
2023-11-07 15:18:59 +0100

    0x01: task 10: basic user object

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 from base64 import b64decode
+from typing import TypeVar
 
 
 class BasicAuth(Auth):
@@ -41,3 +42,17 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return tuple(decoded_base64_authorization_header.split(':', 1))
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """User object from credentials"""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        from models.user import User
+        users = User.search({'email': user_email})
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
