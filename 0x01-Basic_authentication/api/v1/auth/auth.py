5c133ed3d2543bcfb5d95dc6fe1bd311bb05190a
danthecm <etchiedaniel1@gmail.com>
2023-11-07 23:16:36 +0100

    0x01: task 13: require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,8 +14,13 @@ class Auth:
             return True
         if path[-1] != '/':
             path += '/'
-        excluded_paths = [path + '/' if path[-1] !=
-                          '/' else path for path in excluded_paths]
+        excluded_paths = [path + '/' if path[-1] != '/' and path[-1] != "*"
+                          else path for path in excluded_paths]
+        special_paths = [path for path in excluded_paths if '*' in path]
+        if len(special_paths) > 0:
+            for path in special_paths:
+                if path[:-1] in path:
+                    return False
         if path in excluded_paths:
             return False
         return True
