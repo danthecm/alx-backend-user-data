64c8210a56aba8f293e1f4b7999b63cf1db06f5a
danthecm <etchiedaniel1@gmail.com>
2023-11-11 00:58:45 +0100

    0x02: task 4: user id for session id

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -14,8 +14,12 @@ class SessionAuth(Auth):
         """Create a session ID for a user_id"""
         if user_id is None or type(user_id) != str:
             return None
-        session_id = uuid.uuid4()
-        if session_id is None:
-            return None
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return a User ID based on a Session ID"""
+        if session_id is None or type(session_id) != str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
